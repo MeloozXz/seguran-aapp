cdff58faa75fb15b581a2b24757467a394ed681a
MeloozXz <MeloozXz@users.noreply.github.com>
2026-08-03 10:43:58 -0300

    Ajusta base path e adiciona workflow do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/securityproject/',
+  base: '/seguran-aapp/',
   plugins: [
     react(),
     tailwindcss()
